4261c2a98cd91aeba60f7e8b6e6ae6eee2f1ac8f
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-27 16:31:35 +0300

    feat: flex entry point

--- src/sum_in_files/EntryPoint.java
+++ src/sum_in_files/EntryPoint.java
@@ -1,8 +1,14 @@
 package sum_in_files;
 
 public class EntryPoint {
+    private static final int COUNT_FILES = 13;
+    private static final String DIRECTORY = "D:\\Data\\Projects\\JavaConcurrency\\src\\sum_in_files\\files";
+    private static final int LINES_IN_FILES = 5555;
+
     public static void main(String[] args) {
         FileGenerator fileGenerator = new FileGenerator();
-        fileGenerator.createFiles();
+        fileGenerator.createFiles(DIRECTORY, COUNT_FILES);
+
+        fileGenerator.fillFilesWithNumbers(DIRECTORY, COUNT_FILES, LINES_IN_FILES);
     }
 }
