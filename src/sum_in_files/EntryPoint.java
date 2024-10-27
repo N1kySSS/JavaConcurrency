0d9a4a0d337e4c1960729d0b6d1022d0685eff5f
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-27 16:08:04 +0300

    feat: create entry point

---
+++ src/sum_in_files/EntryPoint.java
@@ -0,0 +1,8 @@
+package sum_in_files;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        FileGenerator fileGenerator = new FileGenerator();
+        fileGenerator.createFiles();
+    }
+}
