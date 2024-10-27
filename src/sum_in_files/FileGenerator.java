7a08654b0c35ffb0d2a22eb1d7a9d9fcb0c41116
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-27 16:05:31 +0300

    refactor: naming files beginning 1

--- src/sum_in_files/FileGenerator.java
+++ src/sum_in_files/FileGenerator.java
@@ -6,12 +6,13 @@ import java.io.IOException;
 public class FileGenerator {
 
     private static final int countFiles = 13;
+    private static final File DIRECTORY = new File("D:\\Data\\Projects\\JavaConcurrency\\src\\sum_in_files\\files");
+
 
     private void createFiles() {
-        File directory = new File("D:\\Data\\Projects\\JavaConcurrency\\src\\sum_in_files\\files");
 
-        for(int i = 0; i < countFiles; i++) {
-            File file = new File(directory + "/" + i + ".txt");
+        for(int i = 1; i <= countFiles; i++) {
+            File file = new File(DIRECTORY + "/" + i + ".txt");
             try {
                 if (file.createNewFile()) {
                     System.out.println("Файл " + file.getName() + " создан");
