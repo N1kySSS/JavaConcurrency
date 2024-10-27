e99f2339d5fcdcc991efbfe86485179c05182cba
ImmortalIdiot <max.krasulya@yandex.ru>
2024-10-27 17:29:37 +0300

    feat: separate logic of the main methods

--- src/sum_in_files/EntryPoint.java
+++ src/sum_in_files/EntryPoint.java
@@ -1,14 +1,32 @@
 package sum_in_files;
 
 public class EntryPoint {
-    private static final int COUNT_FILES = 13;
+    private static final int COUNT_FILES = 223;
     private static final String DIRECTORY = "D:\\Data\\Projects\\JavaConcurrency\\src\\sum_in_files\\files";
-    private static final int LINES_IN_FILES = 5555;
+    private static final int LINES_IN_FILES = 99999;
+
+    private static final FileGenerator fileGenerator = new FileGenerator();
+    private static final SingleSummator summator = new SingleSummator();
 
     public static void main(String[] args) {
-        FileGenerator fileGenerator = new FileGenerator();
-        fileGenerator.createFiles(DIRECTORY, COUNT_FILES);
+        clearFiles();
+        createAndFill();
+        outputResult();
+    }
 
+    public static void createAndFill() {
+        fileGenerator.createFiles(DIRECTORY, COUNT_FILES);
         fileGenerator.fillFilesWithNumbers(DIRECTORY, COUNT_FILES, LINES_IN_FILES);
+    }
+
+    public static void clearFiles() {
+        fileGenerator.clearFileContents(DIRECTORY);
+    }
+
+    public static void outputResult() {
+        long start = System.currentTimeMillis();
+        System.out.println(summator.sumAllFiles(DIRECTORY));
+        long end = System.currentTimeMillis();
+        System.out.println("Время выполнения: " + (end - start)/1000.0);
     }
 }
