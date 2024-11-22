acc93e3c23ee1a7c0421855a110b2e1d38a5c4bc
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-23 00:35:18 +0300

    feat: create table class for producers and consumers

---
+++ src/producers_and_consumers/Table.java
@@ -0,0 +1,39 @@
+package producers_and_consumers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Table<ProductType> {
+
+    private final Queue<ProductType> table;
+    private final int capacity;
+
+    public Table(int capacity) {
+        this.table = new LinkedList<>();
+        this.capacity = capacity;
+    }
+
+    public synchronized void produce(ProductType product) throws InterruptedException {
+        while (table.size() == capacity) {
+            wait();
+        }
+
+        table.add(product);
+        System.out.println(Thread.currentThread().getName() + " produced product: " + product + ".\n" +
+                "Product count - " + table.size());
+        notifyAll();
+    }
+
+    public synchronized ProductType consume() throws InterruptedException {
+        while (table.isEmpty()) {
+            wait();
+        }
+
+        ProductType product = table.poll();
+        System.out.println(Thread.currentThread().getName() + " consumed product: " + product + ".\n" +
+                "Product count - " + table.size());
+        notifyAll();
+
+        return product;
+    }
+}
