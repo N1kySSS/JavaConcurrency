166e45a2b4938757afda0d8e7b46446321368509
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-23 00:23:54 +0300

    feat: create consumer

---
+++ src/producers_and_consumers/Consumer.java
@@ -0,0 +1,27 @@
+package producers_and_consumers;
+
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
+public class Consumer<ProductType> implements Runnable {
+
+    private final Table<ProductType> table;
+    private final int timeToConsume;
+
+    public Consumer(Table<ProductType> table, int timeToConsume) {
+        this.table = table;
+        this.timeToConsume = timeToConsume;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                ProductType product = table.consume();
+                MILLISECONDS.sleep(timeToConsume);
+            }
+
+        } catch (InterruptedException interruptedException) {
+            Thread.currentThread().interrupt();
+        }
+    }
+}
