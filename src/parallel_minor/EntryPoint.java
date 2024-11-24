c0427bb4819b84827ad250da5d34d0f3a1612bbb
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-25 01:31:33 +0300

    feat: add the entry point

---
+++ src/parallel_minor/EntryPoint.java
@@ -0,0 +1,59 @@
+package parallel_minor;
+
+import parallel_minor.executors.ParallelExecutor;
+import parallel_minor.executors.SingleThreadExecutor;
+import parallel_minor.util.FindingDeterminantResultSaver;
+import parallel_minor.util.MatrixGenerator;
+import parallel_minor.util.MatrixReader;
+
+import java.math.BigInteger;
+
+public class EntryPoint {
+
+    private static final int matrixOrder = 10;
+    private static final MatrixGenerator matrixGenerator = new MatrixGenerator(matrixOrder);
+
+    public static void main(String[] args) {
+        matrixGenerator.createAndSaveMatrix();
+        singleThreadExecution();
+        recursiveParallelismExecution();
+    }
+
+    public static void singleThreadExecution() {
+        SingleThreadExecutor singleThreadExecutor = new SingleThreadExecutor();
+        int[][] matrix = getMatrix();
+
+        long start = System.currentTimeMillis();
+        BigInteger result = singleThreadExecutor.calculateDeterminant(matrix);
+        long end = System.currentTimeMillis();
+
+        FindingDeterminantResultSaver.save(
+                "SingleThread.txt",
+                end - start,
+                result
+        );
+    }
+
+    public static void recursiveParallelismExecution() {
+        ParallelExecutor parallelExecutor = new ParallelExecutor();
+        int[][] matrix = getMatrix();
+
+        try {
+
+            long start = System.currentTimeMillis();
+            BigInteger result = parallelExecutor.calculateDeterminant(matrix);
+            long end = System.currentTimeMillis();
+
+            FindingDeterminantResultSaver.save("RecursiveParallelism.txt",
+                    end - start,
+                    result);
+        } catch (InterruptedException interruptedException) {
+            System.out.println(interruptedException.getMessage());
+        }
+    }
+
+    private static int[][] getMatrix() {
+        String filePathWithMatrix = matrixGenerator.getFilePath();
+        return MatrixReader.readMatrixFromFile(filePathWithMatrix);
+    }
+}
