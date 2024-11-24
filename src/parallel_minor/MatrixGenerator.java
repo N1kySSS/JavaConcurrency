e606ec401e05f53426d68dd47bf45b04bdd9972e
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-24 21:32:12 +0300

    feat: create matrix generator

---
+++ src/parallel_minor/MatrixGenerator.java
@@ -0,0 +1,83 @@
+package parallel_minor;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Random;
+
+public class MatrixGenerator {
+
+    private final int order;
+    private final Random random = new Random();
+
+    public MatrixGenerator(int order) {
+        this.order = order;
+    }
+
+    private File createMatrixFile() {
+        String directoryPath = "src/parallel_minor/matrix";
+        Path directory = Paths.get(directoryPath);
+
+        try {
+            Files.createDirectories(directory);
+        } catch (IOException ioException) {
+            System.err.println("Ошибка создания пакета: " + ioException.getMessage());
+            return null;
+        }
+
+        Path pathFile = directory.resolve(order + ".txt");
+        File file = new File(pathFile.toString());
+
+        try {
+            if (file.createNewFile()) {
+                System.out.println("Файл " + file.getName() + "успешно создан");
+            }
+        } catch (IOException ioException) {
+            System.err.println("Ошибка создания файла: " + ioException.getMessage());
+            return null;
+        }
+
+        return file;
+    }
+
+    private int[][] generateMatrix() {
+        int[][] matrix = new int[order][order];
+
+        for (int i = 0; i < order; i++) {
+            for (int j = 0; j < order; j++) {
+                matrix[i][j] = random.nextInt(-100, 100);
+            }
+        }
+
+        return matrix;
+    }
+
+    private void saveMatrixToFile(int[][] matrix, String filePath) {
+        File file = new File(filePath);
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (int[] row : matrix) {
+                for (int number : row) {
+                    writer.write(number + " ");
+                }
+
+                writer.newLine();
+            }
+        } catch (IOException ioException) {
+            System.err.println("Ошибка записи в файл: " + ioException.getMessage());
+        }
+    }
+
+    public void createAndSaveMatrix() {
+        File file = createMatrixFile();
+
+        if (file != null) {
+            int[][] matrix = generateMatrix();
+            saveMatrixToFile(matrix, file.getPath());
+        }
+    }
+}
